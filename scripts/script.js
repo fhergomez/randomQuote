251918f7e19d6bcc67558a9bbb6f76f27f3edda8
fhergomez <fhergomez37@gmail.com>
2017-01-30 17:55:03 -0800

    applied random bg color & color to site

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
 
   console.log("I'm ready!!")
-  function getColor() {
-    var color = "#" + Math.floor(Math.random()*16777215).toString(16);
-    return color;
-  };
+
+  // random color
+  var colors = [];
+  for (var i = 0;i < 12;i++) {
+    colors.push("#" + Math.floor(Math.random()*16777215).toString(16)) // toString(16) will convert the number into a hexadecimal value
+  }
+  console.log(colors);
+  var color = Math.floor(Math.random()*colors.length);
+  console.log(color);
 
   function getQuote(){
-    getColor();
+
   };
 
-  $("body").css('background-color', getColor());
-  $("body").css('color', getColor());
+  $("body").css({
+    'background-color': colors[color],
+    'color': colors[color]
+  });
+  // $("body").css('color', getColor());
 
 });
