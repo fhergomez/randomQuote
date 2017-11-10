e760ed4cf7cd96c6945d7809217789257d3bf62a
fhergomez <fhergomez37@gmail.com>
2017-11-09 23:46:46 -0800

    revert ajax call

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         Accept: 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=',
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
       success: function (response) { // retrieves the data as a JSON object
         console.log(response);
         var r = response;
